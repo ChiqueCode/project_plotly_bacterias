2c53defa0236adba1f63051620a19bf2d4a44f5d
Kseniya Bogoslovskaya <fromkseniya@gmail.com>
2019-06-06 20:03:46 -0400

    🐛debugged plotly code for gauge in the bonus part

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -1,57 +1,46 @@
+
 function bonus(sample) {
 
-    /**
-    /* Builds gauge indicating number of belly button scrubs per week
-    /* for given sample.
-    /* @param {string}    sample    Name of the sample
-    */
-  
-    // Construct url for path to data for selected sample
-    var url = "/wfreq/${sample}";
-  
-    // Fetch sample information
-    d3.json(url).then(function(sampleData){  
+    Plotly.d3.json(`/metadata/${sample}`, function(error, metaData) {
+        if (error) return console.warn(error);
+        // Enter the washing frequency between 0 and 180
+        var level = metaData.WFREQ*20;
 
-        // Extract washing frequency
-        var wfreq = sampleData[0];
-
-        // Converting frequency to ratio so it is equivalent to degrees for gauge 
-        var level = wfreq * 180 / 9;
-
-        // Meter point calculus
+        // Calcualting 
         var degrees = 180 - level,
             radius = .5;
         var radians = degrees * Math.PI / 180;
         var x = radius * Math.cos(radians);
         var y = radius * Math.sin(radians);
 
-        // Creating arrow for our gauge
-        var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
+        // Creating a path
+        var mainPath = 'M -.0 -0.05 L .0 0.05 L ',
             pathX = String(x),
             space = ' ',
             pathY = String(y),
             pathEnd = ' Z';
         var path = mainPath.concat(pathX,space,pathY,pathEnd);
 
-        // Constructing our gauge
+        // Defining the data for the Plot (trace)
         var data = [{ type: 'scatter',
         x: [0], y:[0],
-            marker: {size: 28, color:'850000'},
+            marker: {size: 12, color:'850000'},
             showlegend: false,
-            name: 'washings',
-            text: wfreq,
-            hoverinfo: wfreq},
-        { values: [1, 1, 1, 1, 1, 1, 1, 1, 1, 9],
+            name: 'Freq',
+            text: level,
+            hoverinfo: 'text+name'},
+        { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
         rotation: 90,
-        text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3',
-                    '1-2', '0-1', ''],
+        text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
         textinfo: 'text',
         textposition:'inside',
-        marker: {colors:['rgb(131,181,136)', 'rgb(136,188,141)',
-                        'rgb(138,192,134)','rgba(14, 127, 0, .5)', 
-                        'rgba(110, 154, 22, .5)','rgba(170, 202, 42, .5)', 
-                        'rgba(202, 209, 95, .5)','rgba(210, 206, 145, .5)', 
-                        'rgba(232, 226, 202, .5)','rgba(255, 255, 255, 0)']},
+        marker: {
+            colors:[
+                'rgba(0, 105, 11, .5)', 'rgba(10, 120, 22, .5)',
+                'rgba(14, 127, 0, .5)', 'rgba(110, 154, 22, .5)',
+                'rgba(170, 202, 42, .5)', 'rgba(202, 209, 95, .5)',
+                'rgba(210, 206, 145, .5)', 'rgba(232, 226, 202, .5)',
+                'rgba(240, 230, 215, .5)', 'rgba(255, 255, 255, 0)']},
         labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
         hoverinfo: 'label',
         hole: .5,
@@ -59,6 +48,7 @@ function bonus(sample) {
         showlegend: false
         }];
 
+        // Defining the layout 
         var layout = {
         shapes:[{
             type: 'path',
@@ -68,14 +58,16 @@ function bonus(sample) {
                 color: '850000'
             }
             }],
-        title: '<b>Belly button Washing Frequency</b> <br> Scrubs per Week',
-        height: 400,
-        width: 400,
+        title: '<b>Washing Frequency of Belly Button</b> <br> Scrubs per Week',
+        height: 500,
+        width: 500,
         xaxis: {zeroline:false, showticklabels:false,
                     showgrid: false, range: [-1, 1]},
         yaxis: {zeroline:false, showticklabels:false,
                     showgrid: false, range: [-1, 1]}
         };
-        Plotly.newPlot("gauge", data, layout);
+
+        var gauge_plot = document.getElementById('gauge');
+        Plotly.newPlot(gauge_plot, data, layout);
     });
 }
